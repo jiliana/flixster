7b9e8ada886a62d3dafac1ae5da338424c1dc7af
JilianaTiu <jilianatiu@gmail.com>
2021-04-07 14:25:48 -0700

    part2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Yana Tiu on 4/7/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
